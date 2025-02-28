9426096ea98aec1dc52c9ba868106d4d5017e2ac
0xdsqr <dave.dennis@gs.com>
2025-02-27 20:32:23 -0600

    feat(nix): add in falke.nix

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "🟪";
+  
+  inputs = {
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/release-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  
+  outputs = { self, nixpkgs-unstable, nixpkgs-stable, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs-unstable = import nixpkgs-unstable { inherit system; };
+        pkgs-stable = import nixpkgs-stable { inherit system; };
+      in {
+        devShells.default = pkgs-unstable.mkShell {
+          buildInputs = with pkgs-unstable; [
+            nodejs_22
+            bun
+          ];
+          
+          shellHook = ''
+            echo "🟪"
+          '';
+        };
+      });
+}
